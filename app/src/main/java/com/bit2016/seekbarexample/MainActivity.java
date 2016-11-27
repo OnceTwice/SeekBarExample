79db95b8fd40f039f720c5d357be3b4d83c2911b
cho <you-smile@nate.com>
2016-11-28 05:55:54 +0900

    2

--- app/src/main/java/com/bit2016/seekbarexample/MainActivity.java
+++ app/src/main/java/com/bit2016/seekbarexample/MainActivity.java
@@ -3,8 +3,8 @@ package com.bit2016.seekbarexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.DatePicker;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setContentView( R.layout.activity_main );
 
+        ((SeekBar)findViewById(R.id.seekBar)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ((TextView)findViewById(R.id.textView)).setText("Current Volume : " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d("MainActivity", "onStartTrackingTouch() called");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d("MainActivity", "onStopTrackingTouch() called");
+            }
+        });
     }
 }
